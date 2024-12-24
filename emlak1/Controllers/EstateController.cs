9f5841d3daba35c631cb90ad1f3e061ba72e04de
tuba-00 <nur.subasi200272@gmail.com>
2024-12-24 17:58:35 +0300

    initial

---
+++ emlak1/Controllers/EstateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class EstateController : Controller
+{
+    public IActionResult Index()
+    {
+        // Örnek veri
+        var estates = new List<Estate>
+        {
+            new Estate { Id = 1, Category = "Satılık Arsa", Title = "Geniş Arsa", Description = "500 m²", Price = 100000, ImageUrl = "/images/arsa.jpg" },
+            new Estate { Id = 2, Category = "Kiralık Daire", Title = "Şehir Merkezinde", Description = "2+1, 90 m²", Price = 2500, ImageUrl = "/images/daire.jpg" }
+        };
+        return View(estates);
+    }
+}
---
+++ emlak1/Models/Estate.cs
@@ -0,0 +1,9 @@
+﻿public class Estate
+{
+    public int Id { get; set; }
+    public required string Category { get; set; } // "Satılık Arsa", "Kiralık Daire" gibi
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public required string ImageUrl { get; set; }
+}
